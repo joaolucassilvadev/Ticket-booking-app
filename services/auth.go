ef1e72912936ba8ea747130001fcc5e2277a96b0
joaolucassilvadev <js0454261@gmail.com>
2025-10-06 23:06:41 -0300

    feat: add JWT token generation

--- services/auth.go
+++ services/auth.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/joaolucassilvadev/Ticket-booking-app/models"
+	"github.com/joaolucassilvadev/Ticket-booking-app/pkg/utils"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -33,7 +36,43 @@ func (s *AuthService) Login(ctx context.Context, loginData *models.AuthCredentia
 		"exp":  time.Now().Add(time.Hour * 168).Unix(),
 	}
 
-	return user.Email, user, nil
+	token, err := utils.GenerateJWT(claims, jwt.SigningMethodHS256, os.Getenv("JWT_SECRET"))
+	if err != nil {
+		return "", nil, err
+	}
+
+	return token, user, nil
+}
+
+func (s *AuthService) Register(ctx context.Context, RegisterData *models.AuthCredentials) (string, *models.User, error) {
+	if !models.IsValidEmail(RegisterData.Email) {
+		return "", nil, fmt.Errorf("invalid email")
+	}
+	if _, err := s.repository.GetUser(ctx, "email = ?", RegisterData.Email); !errors.Is(err, gorm.ErrRecordNotFound) {
+		return "", nil, fmt.Errorf("email already exists")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(RegisterData.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", nil, err
+	}
+	RegisterData.Password = string(hashedPassword)
+	user, err := s.repository.RegistreUser(ctx, RegisterData)
+	if err != nil {
+		return "", nil, err
+	}
+	claims := jwt.MapClaims{
+		"id":   user.Id,
+		"role": user.Role,
+		"exp":  time.Now().Add(time.Hour * 168).Unix(),
+	}
+
+	token, err := utils.GenerateJWT(claims, jwt.SigningMethodHS256, os.Getenv("JWT_SECRET"))
+	if err != nil {
+		return "", nil, err
+	}
+
+	return token, user, nil
 }
 
 func NewAuthService(repository models.AuthRepository) models.AuthService {
